02d6013c48cde0cb38e22f8d77961bf7fb90ad2f
Juan José Sánchez Medina <gh@jjsm.science>
2026-04-13 14:46:07 -0400

    feat: setup improved

--- src/ipynb_jjsm_tools/setup/dirs.py
+++ src/ipynb_jjsm_tools/setup/dirs.py
@@ -1,5 +1,19 @@
 import os
+import sys
+from pathlib import Path
 
-def create_dirs(data="data", output="graphics"):
+def create_dirs(path, data="data", output="graphics"):
+    path = Path(path)
+    data = path / data
+    output = path / output
     os.makedirs(data, exist_ok=True)
     os.makedirs(output, exist_ok=True)
+
+def add_project_root():
+    current = Path(os.getcwd()).resolve()
+
+    for parent in [current, *current.parents]:
+        if (parent / ".root_ident").exists():
+            if str(parent) not in sys.path:
+                sys.path.insert(0, str(parent))
+            return parent
